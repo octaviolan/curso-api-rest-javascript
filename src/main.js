ee36c70afcb7bf44a1f7ce6d3a6c1ecad3788354
octaviolan <octavio.olan25@gmail.com>
2024-01-09 12:14:59 -0600

    mostrando y ocultando secciones

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ async function getTrendingMoviesPreview() {
   //console.log({data, movies});
   
   movies.forEach(movie => {
-    const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList');
+    const trendingMoviesPreviewList = document.querySelector('#trendingPreview .trendingPreview-movieList');
 
     const movieContainer = document.createElement('div');
     movieContainer.classList.add('movie-container');
@@ -17,7 +17,7 @@ async function getTrendingMoviesPreview() {
     movieImg.setAttribute('src', 'https://image.tmdb.org/t/p/w300/' + movie.poster_path); 
 
     movieContainer.appendChild(movieImg);
-    trendingPreviewMoviesContainer.appendChild(movieContainer);
+    trendingMoviesPreviewList.appendChild(movieContainer);
 
   });
 }
@@ -30,7 +30,7 @@ async function getCategoriesPreview() {
   //console.log({categories});
 
   categories.forEach(category => {
-    const previewCategoriesContainer = document.querySelector('#categoriesPreview .categoriesPreview-list');
+    const categoriesPreviewList = document.querySelector('#categoriesPreview .categoriesPreview-list');
     
     const categoryContainer = document.createElement('div');
     categoryContainer.classList.add('category-container');
@@ -42,7 +42,7 @@ async function getCategoriesPreview() {
 
     categoryTitle.appendChild(categoryTitleText);
     categoryContainer.appendChild(categoryTitle);
-    previewCategoriesContainer.appendChild(categoryContainer)
+    categoriesPreviewList.appendChild(categoryContainer)
   })
   
 }
